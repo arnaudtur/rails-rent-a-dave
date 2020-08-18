69854dd59adb391dfc65c36721bb60988a6b0b38
josephlarue <joseph.larue@edhec.com>
2020-08-18 16:35:13 +0200

    navigation a l'inscription

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
+
+
+  get '/user' => "skills#index", :as => :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :skills, only: [:index, :new, :create, :update, :show]
 
