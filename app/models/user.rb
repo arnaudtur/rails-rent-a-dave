dcc1bc9ed978c467960ebdf10bd7d87b03a372be
josephlarue <joseph.larue@edhec.com>
2020-08-18 09:11:13 +0200

    Began the models and validation rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many :skills
+    has_many :bookings
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :password, presence: true
+    validates :password, uniqueness: true
 end
