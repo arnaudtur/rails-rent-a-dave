e5e0f6a3d00644b281e19272a891d5432dc8a5ff
josephlarue <joseph.larue@edhec.com>
2020-08-18 11:50:26 +0200

    models

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,8 @@
 class Skill < ApplicationRecord
+  SKILLS = ["Python","Java","Ruby/Ruby on rails","HTML","Javascript","C","C++","C#","PHP","SQL","Swift","CSS","Visual Basic","Product Management","R","R++"]
   belongs_to :user
+  validates :name, presence: true, 
+  validates :name, inclusion: { in: SKILLS,
+    message: "Skill not in the skill list" }
+  validates :price, presence: true, numericality: true
 end
