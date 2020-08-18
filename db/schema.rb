686b8a6fc13f061bac4fdf1978657bd786f55530
josephlarue <joseph.larue@edhec.com>
2020-08-18 12:39:41 +0200

    db update

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_092014) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
