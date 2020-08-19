b94372623d941be19e051c48c2333e31ef42c8b4
thibaut morla <thibaut.morla@gmail.com>
2020-08-19 17:26:08 +0200

    show design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,19 +34,19 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 
-// JavaScript code 
-function search_skills() { 
-  let input = document.getElementById('table_filter').value 
-  input=input.toLowerCase(); 
-  let x = document.getElementsByClassName('name'); 
-    
-  for (i = 0; i < x.length; i++) {  
-      if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-          x[i].style.display="none"; 
-      } 
-      else { 
-          x[i].style.display="list-item";                  
-      } 
-  } 
+// JavaScript code
+function search_skills() {
+  let input = document.getElementById('table_filter').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('name');
+
+  for (i = 0; i < x.length; i++) {
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";
+      }
+  }
 }
 
