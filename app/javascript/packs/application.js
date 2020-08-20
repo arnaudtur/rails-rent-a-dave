673232cf2ab193d0d726f9d6c02dbf48c413f103
thibaut morla <thibaut.morla@gmail.com>
2020-08-20 16:44:01 +0200

    navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -58,3 +58,20 @@ document.addEventListener('turbolinks:load', () => {
   loadDynamicBannerText();
 });
 
+$(document).ready(function() {
+           $(".menu-icon").on("click", function() {
+                 $("nav ul").toggleClass("showing");
+           });
+     });
+
+     // Scrolling Effect
+
+     $(window).on("scroll", function() {
+           if($(window).scrollTop()) {
+                 $('nav').addClass('black');
+           }
+
+           else {
+                 $('nav').removeClass('black');
+           }
+     })
