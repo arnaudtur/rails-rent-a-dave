b5e792ba0fee70402957a6e6f38141d278993b4d
thibaut morla <thibaut.morla@gmail.com>
2020-08-21 10:50:29 +0200

    fix navbar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,13 +4,13 @@ class BookingsController < ApplicationController
       @bookings = Booking.all
       policy_scope(Booking)
     end
-    
+
     def new
         @booking = Booking.new
         @skill = Skill.find(params[:skill_id])
         authorize @booking
     end
-    
+
       def create
         @booking = Booking.new(params_bookings)
         @booking.skill = Skill.find(params[:skill_id])
@@ -22,41 +22,41 @@ class BookingsController < ApplicationController
             # render 'new'
           end
         end
-   
-    
+
+
       def show
         @booking = Booking.find(params[:id])
         authorize @booking
       end
-    
+
       def destroy
         @booking = Booking.find(params[:id])
         @booking.destroy
         authorize @booking
-    
+
         # no need for app/views/restaurants/destroy.html.erb
         redirect_to skills_path
       end
-    
-    
+
+
       def edit
         @booking = Booking.find(params[:id])
         authorize @booking
       end
-    
+
       def update
         @booking = Booking.find(params[:id])
         @booking.update(params_bookings)
         authorize @booking
-    
+
         # no need for app/views/restaurants/update.html.erb
         redirect_to skill_path(@skill)
       end
-    
-    
-    
+
+
+
       private
-    
+
       def params_bookings
         params.require(:booking).permit(:start_date, :end_date, :status)
       end
