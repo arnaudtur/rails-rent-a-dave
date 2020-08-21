f8e492c44c655fe838c70194471499d3c2616b31
Arnaud Turnani <arnaud.tur@live.fr>
2020-08-21 14:37:08 +0200

    Skill Index Fix

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,6 +3,7 @@ class Skill < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
+  has_one :photo, through: :users
   validates :name, presence: true
   validates :name, inclusion: { in: ["Python","Java","Ruby/Ruby on rails","HTML","Javascript","C","C++","C#","PHP","SQL","Swift","CSS","Visual Basic","Product Management","R","R++"],
     message: "%{value} is not in the list" }
