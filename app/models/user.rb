33154a85af1a5eaa14be006da71d7bef1ca89aac
Arnaud Turnani <arnaud.tur@live.fr>
2020-08-21 12:14:23 +0200

    cloudinary add

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :skills
-    has_many :bookings
-    validates :name, presence: true
-    validates :name, uniqueness: true
-    validates :email, presence: true
-    validates :email, uniqueness: true
-    validates :password, presence: true
-    validates :password, uniqueness: true
+  has_many :skills
+  has_many :bookings
+  has_one_attached :photo
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true
+  validates :password, uniqueness: true
 end
